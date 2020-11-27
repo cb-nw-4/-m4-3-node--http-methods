475dd2d74d907965ea0bf2978a6fdb4ef2814137
Joni Sadler <joni.anne.sadler@gmail.com>
2020-11-26 20:11:58 -0500

    Form validation working

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const { stock, customers } = require("./data/inventory");
 
 express()
   // Below are methods that are included in express(). We chain them for convenience.
@@ -18,6 +20,30 @@ express()
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
+
+  .post('/order', (req, res) => {
+    console.log(req.body);
+    const country = req.body.country;
+    const givenName = req.body.givenName;
+    const surname = req.body.surname;
+    const email = req.body.email;
+    const address = req.body.address;
+    const order = req.body.order;
+    const size = req.body.size;
+
+    if (country !== "Canada") {
+      res.json({"status": "error", "error": "undeliverable"});
+    } else if ((givenName === customers.givenName && surname === customers.surname) || email === customers.email || address === customers.address) {
+      res.json({"status": "error", "error": "repeat-customer"});
+    } else if (email.length < 5 || (!email.includes("@"))) {
+      res.json({"status": "error", "error": "missing-data"});
+    } else if (order === "tshirt" && size === "medium") {
+      res.json({"status": "error", "error": "unavailable"});
+    } else {
+      res.json({"status": "success"});
+    }
+
+  })
 
   // add new endpoints here ☝️
   // ---------------------------------

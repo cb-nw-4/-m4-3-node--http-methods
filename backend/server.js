158883c9d70f3a8cd84161b3563f4779da7d6074
sfillionraff <sfillionraff@gmail.com>
2020-11-28 14:25:11 -0500

    node #3

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const { customers, stock } = require("./data/inventory.js"); 
 
 express()
   // Below are methods that are included in express(). We chain them for convenience.
@@ -18,6 +21,70 @@ express()
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
+  .post ('/order', (req, res) => {
+    // deconstruct req.body to access properties
+    const {
+      order,
+      size,
+      givenName, 
+      surname,
+      email, 
+      address,
+      city,
+      province,
+      country
+    } = req.body
+    // check if customer's name, email, or address already exists
+    const customerFound = customers.find((customer) => {
+      return customer.givenName === givenName || customer.email === email || customer.address === address;
+    });
+    // check if email is valid
+    let emailValid = (email) => {
+      return /\S+@\S+\.\S+/.test(email);
+    }
+
+    // check if any form data is missing
+    let dataIsAvailable = () => {
+      return surname && givenName && email && address && province && country
+    };
+
+    // check if item is in stock
+    let stockIsAvailable = () => {
+      return stock[order] && (stock[order] != 0 || stock[order][size] != 0);
+    }
+
+    if(customerFound){
+      res.status(400).send({
+        status: "error",
+        error:  "repeat-customer"
+      })
+    // check if country selected is Canada or if any information is missing
+    } else if (country.toLowerCase() !== 'canada'){
+      res.status(400).send({
+        status: "error",
+        error: "undeliverable"
+      })
+    } else if (!emailValid(email)){
+      res.status(400).send({
+        status: "error",
+        error: "email is not valid"
+      })
+    } else if (!dataIsAvailable()) {
+      res.status(400).send({
+        status: "error",
+        error: "missing-data"
+      })
+    } else if (!stockIsAvailable()) {
+      res.status(400).send({
+        status: "error",
+        error: "unavailable"
+      })
+    } else{
+      res.status(200).send({
+        status: "success",
+      })
+    }
+  })
 
   // add new endpoints here ☝️
   // ---------------------------------
